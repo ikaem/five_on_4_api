66f37e3c528ef98cc8091db546ba00cf337e36f6
Karlo M <karlo.marinovic@decode.agency>
2024-03-03 10:28:24 +0100

    add credentials

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,17 @@
 import express from 'express';
+import cors from 'cors';
 import { AppRouter } from './features/core/presentation/router/app.router.js';
+import { EnvironmentVariablesProvider } from './wrappers/env-var-provider/env-var-provider.js';
 
 export const getApp = (appRouter: AppRouter) => {
   const app = express();
+  app.use(
+    cors({
+      // TDOO test
+      origin: 'http://localhost:3000',
+      credentials: true,
+    }),
+  );
   app.use(express.json());
   app.use('/api', appRouter.router);
 
