f25b0e01741ee9f99f19c8669c314c477ef84820
Karlo M <karlo.marinovic@decode.agency>
2024-03-05 16:18:49 +0100

    create create auth logic in data source

--- src/features/auth/data/entities/auth.entity.ts
+++ src/features/auth/data/entities/auth.entity.ts
@@ -1,11 +1,21 @@
 // TODO will reuse this for typeorm
 export class AuthEntity {
-  constructor(id: number, nickname: string, email: string, name: string, avatarUrl: string) {
+  constructor(
+    id: number,
+    nickname: string,
+    email: string,
+    name: string,
+    avatarUrl: string,
+    password: string | undefined,
+    authType: string,
+  ) {
     this.id = id;
     this.nickname = nickname;
     this.email = email;
     this.name = name;
     this.avatarUrl = avatarUrl;
+    this.password = password;
+    this.authType = authType;
   }
 
   id: number;
@@ -13,4 +23,6 @@ export class AuthEntity {
   email: string;
   name: string;
   avatarUrl: string;
+  password: string | undefined;
+  authType: string;
 }
